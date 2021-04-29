05be3ae7644b3b1fa30ff557e1fdca2c637052cc
AdrielIg <adrielignaciogomez@gmail.com>
2021-04-29 01:08:36 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { useState } from 'react';
+import { useChat } from './components/useChat';
+import { db } from './Firebase';
+
 function App() {
+
+  const [messageText, setMessage] = useState("")
+  const { loading, messages, error } = useChat()
+
+
+  const onSendMessage = (e) => {
+    e.preventDefault()
+
+    db.collection("messages").add({ timestamp: Date.now(), message: messageText })
+
+
+    setMessage("")
+  }
+
+
+  const messageOrder = messages.map(mes => mes.timestamp)
+
+
+
+  function compare(a, b) {
+    if (a.timestamp < b.timestamp) {
+      return 1;
+    }
+    if (a.timestamp > b.timestamp) {
+      return -1;
+    }
+    return 0;
+  }
+
+  messages.sort(compare);
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Adro-chat</h1>
+      <p>Escribi y sorprendete rey</p>
+      <div className="display">
+        <ul>
+          {messages.map(m => <li key={m.id}>{m.message}</li>)}
+        </ul>
+      </div>
+      <form>
+        <input placeholder="Escribe algo..." value={messageText} onChange={(e) => setMessage(e.target.value)} />
+        <button type="submit" onClick={onSendMessage}>Enviar</button>
+      </form>
     </div>
   );
 }
